ae0dea565eef504e315b68aa1540430779abe64c
rakibulislam8226 <rakibulislam8226@gmail.com>
2023-01-21 11:19:36 +0600

    spread and rest operator added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import welcome from "./functions/welcome.js";
 import createBlog from './arrow_functions/blog.js'
 
-// inherite form another // 
+// // inherite form another // 
 // welcome();
 // createBlog('blog title', 'blog body');
 
@@ -11,15 +11,16 @@ Demo = 'Rakib';
 
 let fname = 'rakib';
 let lname = 'khan';
+
 // let age = prompt('Enter ' + fname + ' age: ');
 
 
-// using template string //
+// // using template string //
 // let result = `${fname} ${lname} your age ${age}.`
 // console.log(result)
 
 
-// arrow function and setTimeout with this keyword //
+// // arrow function and setTimeout with this keyword //
 // let nepal = {
 //     mountains: ['Dilli', 'Ctg', 'Demo'],
 //     printWithDash: function () {
@@ -30,25 +31,61 @@ let lname = 'khan';
 // nepal.printWithDash();
 
 
-// Object destructuring in ES6 //
-let thingsTodo = {
-    morning: 'Breakfast',
-    afternoon: 'Coding',
-    night: ['sleep', 'Dream']
-};
-let { morning, afternoon } = thingsTodo;
+// // Object destructuring in ES6 //
+// let thingsTodo = {
+//     morning: 'Breakfast',
+//     afternoon: 'Coding',
+//     night: ['sleep', 'Dream']
+// };
+// let { morning, afternoon } = thingsTodo;
 // console.log(morning +' - '+ afternoon);
 
 
-let studentUni = (student) => {
-    let { name, university } = student;
-    // console.log(`${name} from ${university}`);
-}
-studentUni({
-    name: 'rakib',
-    university: 'GUB'
-});
+// let studentUni = (student) => {
+//     let { name, university } = student;
+//     // console.log(`${name} from ${university}`);
+// }
+// studentUni({
+//     name: 'rakib',
+//     university: 'GUB'
+// });
 
-// array destructuring in ES6 //
-let [ , findOne]=['first', 'second', 'thired']; //findOne one indicate the array number and have to index it with (,)
-console.log(findOne);
+// // array destructuring in ES6 //
+// let [ , findOne]=['first', 'second', 'thired']; //findOne one indicate the array number and have to index it with (,)
+// console.log(findOne);
+
+
+// // Restructuring
+// let f_name = 'rakib';
+// let l_name = 'khan';
+
+// let nameCombination = {f_name, l_name};
+// console.log(nameCombination);
+
+// // Spread operator  (...)
+// let dhaka = ['mirpur', 'banani', 'uttora'];
+// let khulna = ['jessore', 'narayil'];
+// let total = [...dhaka, ...khulna];
+// // console.log(total);
+
+// // rest operator  (...)
+// let [first, ...rest] = dhaka;
+// console.log(first);
+// console.log(...rest);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
